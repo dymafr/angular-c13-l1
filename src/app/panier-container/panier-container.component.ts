d66d624a4c26225c54dc1a1b8cb347250858559d
dymafr <erwan1309@hotmail.com>
2021-09-07 15:17:05 +0200

    2 files modified

--- src/app/panier-container/panier-container.component.ts
+++ src/app/panier-container/panier-container.component.ts
@@ -1,28 +1,26 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { Ingredient } from '../shared/interfaces/ingredient.interface';
-import { PanierService } from '../shared/services/panier.service';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { Ingredient } from "../shared/interfaces/ingredient.interface";
+import { PanierService } from "../shared/services/panier.service";
 
 @Component({
-  selector: 'app-panier-container',
-  templateUrl: './panier-container.component.html',
-  styleUrls: ['./panier-container.component.scss']
+  selector: "app-panier-container",
+  templateUrl: "./panier-container.component.html",
+  styleUrls: ["./panier-container.component.scss"]
 })
 export class PanierContainerComponent implements OnInit, OnDestroy {
-  public ingredients?: Ingredient[];
-  public subscription: Subscription = new Subscription();
+  public ingredients?: Ingredient[] | null;
+  public subscription?: Subscription;
 
   constructor(private panierService: PanierService) {}
 
   ngOnInit() {
-    this.subscription.add(
-      this.panierService.ingredients$.subscribe(
-        (ingredients: Ingredient[]) => (this.ingredients = ingredients)
-      )
+    this.subscription = this.panierService.ingredients$.subscribe(
+      (ingredients: Ingredient[] | null) => (this.ingredients = ingredients)
     );
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.subscription?.unsubscribe();
   }
 }
